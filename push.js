c0adb0e7945d7bc4644498035f33ac590a70da2a
shakediki <146827492+shakediki@users.noreply.github.com>
2023-10-03 13:47:40 +0300

    Update push.js

--- push.js
+++ push.js
@@ -9,6 +9,10 @@ console.log(vapidKeys);
 
 push.setVapidDetails('mailto:shakediki@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/c91c3555ixw:APA91bFJD5CW2OwB3n0oucidFmVsD753yyZjTl2XqURAUY4nR7SyVwSGTKi3e3e_4PxTNkc5D8G7FYL_6x8MtLrsEsVFpr8SL5YO6i64cM38Y_0roecTJEVFlNCf69plOle7cg0iENF7","expirationTime":null,"keys":{"p256dh":"BOUhi3WhyVSRgDDzZCoH5vwm3y0HeWDuP_vkHbtpiT7Xqp3wmhYwbZ5bF99I0scMvVf3-WCKcEQhGK8mTRzy5GM","auth":"ep5XnUISXQ143D26cy96Yw"}} //the 
+let sub = {endpoint:
+  "https://fcm.googleapis.com/fcm/send/dXABIsk1_jc:APA91bEr1Qr1qD1sKZer0Bd3u44HGIw_qeagrwQhb6s5sqXzd_LJ1v4zovOGOtkckzdf_fIEuFK_otKeAUJLL3Na4d5AF-FpMEPRR_PmC0d9aUj1cgCa2cLjZjZPOMBVPbWqkVJWMgJ2",
+  expirationTime:null,
+  keys: 
+  {p256dh:"BAWbjWphCM8Po7h-yQ6yyJ-ByfGItJOMcl4uafJuNQrzVi4uU8E0om2OjfhVeDJqIujVmfAevzEnDoQ2CFF99b4",auth:"6YlytpciMd5YTGFZx5qSKA"}} //the 
 
 push.sendNotification(sub, 'test message')
