0bab1e67ac0a2a8f5eeb83db354dc349f1cb1d48
shakediki <146827492+shakediki@users.noreply.github.com>
2023-10-03 13:39:53 +0300

    Update push.js

--- push.js
+++ push.js
@@ -9,6 +9,6 @@ console.log(vapidKeys);
 
 push.setVapidDetails('mailto:shakediki@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {} //the 
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/c91c3555ixw:APA91bFJD5CW2OwB3n0oucidFmVsD753yyZjTl2XqURAUY4nR7SyVwSGTKi3e3e_4PxTNkc5D8G7FYL_6x8MtLrsEsVFpr8SL5YO6i64cM38Y_0roecTJEVFlNCf69plOle7cg0iENF7","expirationTime":null,"keys":{"p256dh":"BOUhi3WhyVSRgDDzZCoH5vwm3y0HeWDuP_vkHbtpiT7Xqp3wmhYwbZ5bF99I0scMvVf3-WCKcEQhGK8mTRzy5GM","auth":"ep5XnUISXQ143D26cy96Yw"}} //the 
 
 push.sendNotification(sub, 'test message')
